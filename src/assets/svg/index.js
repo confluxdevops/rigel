22077865da65727edfa82e23673009225655a380
angelia <greenfish84@126.com>
2021-05-13 17:42:26 +0800

    feat: dropdown

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,3 +1,4 @@
 export {ReactComponent as Close} from './close.svg'
 export {ReactComponent as ArrowRight} from './arrow-right.svg'
+export {ReactComponent as ArrowDown} from './arrow-down.svg'
 export {ReactComponent as Loading} from './loading.svg'
