36bda1363ce0f55fe5ca58d9f35ca504a359f4e6
chao.zhou <zctocm@gmail.com>
2021-05-25 16:42:46 +0800

    feat(router): add router

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 import {useState, Suspense} from 'react'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import {ReactComponent as Logo} from '../logo.svg'
 import {logo} from '../assets/images'
 import './App.css'
@@ -16,6 +17,12 @@ import {
 } from '../components'
 import {TransactionReceiptionModal} from './components'
 import {requestSponsor} from './../utils/request'
+
+/**
+ * Page
+ */
+import ShuttlePage from './Shuttle/index'
+
 function App() {
   const [open, setOpen] = useState(false)
   requestSponsor('getTokenList', ['eth']).then(res => {
@@ -75,72 +82,83 @@ function App() {
 
   return (
     <Suspense fallback={null}>
-      <div className="container mx-auto h-full flex justify-center items-center">
-        <header className="App-header">
-          <p className="text-black">
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <Router>
+        <div className="container mx-auto h-full flex justify-center items-center">
+          <header className="App-header">
+            <p className="text-black">
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <img src={logo} alt="logo" />
+            <Logo className="text-primary" />
+            <Loading />
+            <Button disabled onClick={info}>
+              Message
+            </Button>
+            <Button
+              onClick={openNotification}
+              startIcon={<Logo className=" text-white" />}
+              className="mb-2"
+            >
+              Notification
+            </Button>
+            <Input />
+            <Tag closable color="error">
+              Max
+            </Tag>
+            <div>
+              <TokenType token={token} type="from" chain="eth"></TokenType>
+            </div>
+            <div>
+              <TokenType token={token} type="to" chain="cfx"></TokenType>
+            </div>
+            <TransactionReceiptionModal
+              type="success"
+              open={false}
+              fromChain="cfx"
+              toChain="eth"
+              fromToken={{symbol: 'CFX'}}
+              toToken={{symbol: 'eCFX'}}
+              value="23.68"
+            />
+            <Modal
+              open={open}
+              title="Tips"
+              onClose={() => setOpen(false)}
+              content="Get incoming transactions Third party APIs are used to show your incoming transactions in the history. Turn off if you don’t want us to pull data from those services."
+              actions={
+                <Button fullWidth onClick={() => setOpen(false)}>
+                  Get
+                </Button>
+              }
+            />
+          </header>
+          <Dropdown
+            overlay={menu}
+            placement="bottomLeft"
+            trigger={['click']}
+            arrow
           >
-            Learn React
-          </a>
-          <img src={logo} alt="logo" />
-          <Logo className="text-primary" />
-          <Loading />
-          <Button disabled onClick={info}>
-            Message
-          </Button>
-          <Button
-            onClick={openNotification}
-            startIcon={<Logo className=" text-white" />}
-            className="mb-2"
-          >
-            Notification
-          </Button>
-          <Input />
-          <Tag closable color="error">
-            Max
-          </Tag>
+            <Button onClick={e => e.preventDefault()}>Hover me</Button>
+          </Dropdown>
           <div>
-            <TokenType token={token} type="from" chain="eth"></TokenType>
+            <Link to="/shuttle">Go to Shuttle</Link>
           </div>
-          <div>
-            <TokenType token={token} type="to" chain="cfx"></TokenType>
-          </div>
-          <TransactionReceiptionModal
-            type="success"
-            open={false}
-            fromChain="cfx"
-            toChain="eth"
-            fromToken={{symbol: 'CFX'}}
-            toToken={{symbol: 'eCFX'}}
-            value="23.68"
-          />
-          <Modal
-            open={open}
-            title="Tips"
-            onClose={() => setOpen(false)}
-            content="Get incoming transactions Third party APIs are used to show your incoming transactions in the history. Turn off if you don’t want us to pull data from those services."
-            actions={
-              <Button fullWidth onClick={() => setOpen(false)}>
-                Get
-              </Button>
-            }
-          />
-        </header>
-        <Dropdown
-          overlay={menu}
-          placement="bottomLeft"
-          trigger={['click']}
-          arrow
-        >
-          <Button onClick={e => e.preventDefault()}>Hover me</Button>
-        </Dropdown>
-      </div>
+          <Route exact strict path="/shuttle" component={ShuttlePage}></Route>
+        </div>
+        <Switch>
+          <Route path="/shuttle">
+            <ShuttlePage />
+          </Route>
+        </Switch>
+      </Router>
     </Suspense>
   )
 }
