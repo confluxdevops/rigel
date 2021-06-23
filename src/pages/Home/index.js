edea12f8624a985910032bd4ffb03db3d0d0798a
Alice <tianrun@confluxnetwork.org>
2021-06-23 13:58:32 +0800

    feat(home): home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,27 @@
 import {useEffectOnce} from 'react-use'
 import {useTranslation} from 'react-i18next'
-import shuttleflow from '../../assets/img/SHUTTLEFLOW.png'
-import tokenleft from '../../assets/img/token-left.png'
-import sfcenter from '../../assets/img/sf-center.png'
-import tokenright from '../../assets/img/token-right.png'
-import copyright from '../../assets/img/copyright.png'
-import twitter from '../../assets/img/twitter.png'
-import telegram from '../../assets/img/telegram.png'
-import discord from '../../assets/img/discord.png'
-import medium from '../../assets/img/medium.png'
-import github from '../../assets/img/github.png'
-import lightleft from '../../assets/img/light-left.png'
-import lightright from '../../assets/img/light-right.png'
-import lightcenter from '../../assets/img/light-center.png'
-import lighttunnel1 from '../../assets/img/light-tunnel1.png'
-import lighttunnel2 from '../../assets/img/light-tunnel2.png'
-import pipleleft from '../../assets/img/piple-left.png'
-import pipleright from '../../assets/img/piple-right.png'
-import baseleft from '../../assets/img/base-left.png'
-import baseright from '../../assets/img/base-right.png'
-import basecenter from '../../assets/img/base-center.png'
-import tunnelleft from '../../assets/img/tunnel-left.png'
-import tunnelright from '../../assets/img/tunnel-right.png'
+import {BaseCenter} from '../../assets/img'
+import {BaseLeft} from '../../assets/img'
+import {BaseRight} from '../../assets/img'
+import {ShuttleFlow} from '../../assets/img'
+import {TokenLeft} from '../../assets/img'
+import {SfCenter} from '../../assets/img'
+import {TokenRight} from '../../assets/img'
+import {CopyRight} from '../../assets/img'
+import {Twitter} from '../../assets/img'
+import {Telegram} from '../../assets/img'
+import {Discord} from '../../assets/img'
+import {Medium} from '../../assets/img'
+import {GitHub} from '../../assets/img'
+import {LightLeft} from '../../assets/img'
+import {LightRight} from '../../assets/img'
+import {LightCenter} from '../../assets/img'
+import {LightTunnel1} from '../../assets/img'
+import {LightTunnel2} from '../../assets/img'
+import {PipleLeft} from '../../assets/img'
+import {PipleRight} from '../../assets/img'
+import {TunnelLeft} from '../../assets/img'
+import {TunnelRight} from '../../assets/img'
 
 function Home() {
   useEffectOnce(() => {
@@ -36,12 +36,8 @@ function Home() {
     <div className="w-full min-h-15 h-screen relative">
       <div className="w-360">
         <div className="ml-24">
-          <div className="pt-24">
-            <img className="w-160" src={shuttleflow} alt="title" />
-          </div>
-          <div className="mt-9">
-            <p className="text-gray-400 text-xl">{t('home.subTitle')}</p>
-          </div>
+          <img className="w-160 pt-24" src={ShuttleFlow} alt="title" />
+          <p className="text-gray-400 mt-9 text-xl">{t('homePage.subTitle')}</p>
           <div className="mt-9 flex">
             <a
               className="bg-primary w-32 h-10 rounded-sm flex justify-center items-center mr-5"
@@ -49,7 +45,7 @@ function Home() {
               rel="noreferrer"
               target="_blank"
             >
-              <p className="text-white text-sm">{t('home.ShuttleFlow')}</p>
+              <p className="text-white text-sm">{t('homePage.ShuttleFlow')}</p>
             </a>
             <a
               className="bg-primary w-24 h-10 rounded-sm flex justify-center items-center"
@@ -61,13 +57,13 @@ function Home() {
               rel="noreferrer"
               target="_blank"
             >
-              <p className="text-white text-sm">{t('home.LightPaper')}</p>
+              <p className="text-white text-sm">{t('homePage.LightPaper')}</p>
             </a>
           </div>
         </div>
         <div className="relative">
           <div className="absolute left-240 top-30">
-            <img className="w-64" src={tunnelright} alt="tunnel" />
+            <img className="w-64" src={TunnelRight} alt="tunnel" />
           </div>
           <svg
             className="absolute w-80 h-52 left-232 top-24"
@@ -107,20 +103,18 @@ function Home() {
               />
             </image>
           </svg>
-          <div className="absolute left-120 top-9">
-            <img className="w-168" src={basecenter} alt="base" />
+          <div className="absolute left-136 top-9">
+            <img className="w-168" src={BaseCenter} alt="base" />
           </div>
           <div className="absolute left-260 top-2.5">
-            <img className="w-110" src={baseright} alt="base" />
+            <img className="w-110" src={BaseRight} alt="base" />
           </div>
           <div className="absolute left-65 top-60">
-            <img src={tunnelleft} alt="tunnel" />
+            <img src={TunnelLeft} alt="tunnel" />
           </div>
-          <div className="absolute left-48 top-48 animate-bounce">
-            <img src={tokenleft} alt="token" />
-          </div>
+
           <svg
-            className="absolute left-44 top-48 w-120 h-52 "
+            className="absolute left-44 top-48 w-132 h-52 "
             viewBox="0 0 200 100"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -158,41 +152,42 @@ function Home() {
             </image>
           </svg>
           <div className="absolute left-1 top-56">
-            <img className="w-104" src={baseleft} alt="base" />
+            <img className="w-104" src={BaseLeft} alt="base" />
+          </div>
+          <div className="absolute left-48 top-48 animate-bounce">
+            <img src={TokenLeft} alt="token" />
           </div>
           <div className="absolute left-192 top-4 animate-bounce">
-            <img className="w-40" src={sfcenter} alt="sf" />
+            <img className="w-40" src={SfCenter} alt="sf" />
           </div>
           <div className="absolute left-302 -top-6 animate-bounce">
-            <img src={tokenright} alt="token" />
+            <img src={TokenRight} alt="token" />
           </div>
           <div className="absolute left-40 top-52 animate-pulse">
-            <img src={lightleft} alt="light" />
+            <img src={LightLeft} alt="light" />
           </div>
           <div className="absolute left-154 -top-1 animate-pulse">
-            <img src={lightcenter} alt="light" />
+            <img src={LightCenter} alt="light" />
           </div>
           <div className="absolute left-293 -top-1 animate-pulse">
-            <img src={lightright} alt="light" />
+            <img src={LightRight} alt="light" />
           </div>
           <div className="absolute left-158 top-60 animate-pulse">
-            <img src={lighttunnel1} alt="light" />
+            <img src={LightTunnel1} alt="light" />
           </div>
           <div className="absolute left-280 top-34 animate-pulse">
-            <img src={lighttunnel2} alt="light" />
+            <img src={LightTunnel2} alt="light" />
           </div>
           <div className="absolute left-116 top-64">
-            <img src={pipleleft} alt="piple" />
+            <img src={PipleLeft} alt="piple" />
           </div>
           <div className="absolute left-270 top-44">
-            <img src={pipleright} alt="piple" />
+            <img src={PipleRight} alt="piple" />
           </div>
         </div>
       </div>
       <div className="absolute bottom-8 w-full flex justify-between py-4 border-solid border-t-2 border-gray-20">
-        <div>
-          <img src={copyright} alt="copyright" />
-        </div>
+        <img src={CopyRight} alt="copyright" />
         <div className="flex">
           <a
             className="mr-5"
@@ -200,7 +195,7 @@ function Home() {
             rel="noreferrer"
             target="_blank"
           >
-            <img src={twitter} alt="twitter" />
+            <img src={Twitter} alt="twitter" />
           </a>
           <a
             className="mr-5"
@@ -208,7 +203,7 @@ function Home() {
             rel="noreferrer"
             target="_blank"
           >
-            <img src={telegram} alt="telegram" />
+            <img src={Telegram} alt="telegram" />
           </a>
           <a
             className="mr-5"
@@ -216,7 +211,7 @@ function Home() {
             rel="noreferrer"
             target="_blank"
           >
-            <img src={discord} alt="discord" />
+            <img src={Discord} alt="discord" />
           </a>
           <a
             className="mr-5"
@@ -224,7 +219,7 @@ function Home() {
             rel="noreferrer"
             target="_blank"
           >
-            <img src={medium} alt="medium" />
+            <img src={Medium} alt="medium" />
           </a>
           <a
             className="mr-5"
@@ -232,7 +227,7 @@ function Home() {
             rel="noreferrer"
             target="_blank"
           >
-            <img src={github} alt="github" />
+            <img src={GitHub} alt="github" />
           </a>
         </div>
       </div>
