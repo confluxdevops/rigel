7ffcc9c35449aff105caa8b4e312436616fac49e
Alice <tianrun@confluxnetwork.org>
2021-06-28 14:11:50 +0800

    feat(maintenance): remove mobile footer from home and maintenance

--- src/pages/components/MobileFooter/index.js
+++ src/pages/components/MobileFooter/index.js
@@ -1,7 +1,9 @@
+import {useLocation} from 'react-use'
 import {WalletHub, LanguageButton, ThemeButton} from '../../components'
 import {useConnectData} from '../../../hooks'
 
 function MobileFooter() {
+  const {pathname} = useLocation()
   const connectData = useConnectData()
   const pendingTransactions = [
     {
@@ -13,18 +15,22 @@ function MobileFooter() {
     {type: 'approve', tokenSymbol: 'UNI'},
   ]
 
-  return (
-    <div className="bg-gray-0 h-16 shadow-common w-full rounded-tl-2.5xl rounded-tr-2.5xl px-3 flex items-center justify-between">
-      <WalletHub
-        connectData={connectData}
-        pendingTransactions={pendingTransactions}
-      />
-      <div className="flex items-center">
-        <ThemeButton />
-        <LanguageButton />
+  if (pathname === '/' || pathname === '/maintenance') {
+    return null
+  } else {
+    return (
+      <div className="bg-gray-0 h-16 shadow-common w-full rounded-tl-2.5xl rounded-tr-2.5xl px-3 flex items-center justify-between">
+        <WalletHub
+          connectData={connectData}
+          pendingTransactions={pendingTransactions}
+        />
+        <div className="flex items-center">
+          <ThemeButton />
+          <LanguageButton />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default MobileFooter
