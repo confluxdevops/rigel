6becbd7b3c80e1a8cc0035838b5e772155410d12
chao.zhou <zctocm@gmail.com>
2021-07-07 15:50:21 +0800

    fix(i18n): en-US en

--- src/pages/components/LanguageButton/index.js
+++ src/pages/components/LanguageButton/index.js
@@ -1,14 +1,13 @@
 import {useTranslation} from 'react-i18next'
 import {EnglishIcon, ChineseIcon} from '../../../assets/svg'
 import {WrapIcon} from '../../../components'
-
 function LanguageButton() {
   const {i18n} = useTranslation()
   const {language} = i18n
   const onChangeLanguage = () => {
-    if (language === 'en') {
+    if (language.indexOf('en') !== -1) {
       i18n.changeLanguage('zh-CN')
-    } else if (language === 'zh-CN') {
+    } else if (language.indexOf('zh') !== -1) {
       i18n.changeLanguage('en')
     }
   }
@@ -20,7 +19,7 @@ function LanguageButton() {
       size="w-7 h-7"
       onClick={onChangeLanguage}
     >
-      {language === 'en' ? (
+      {language.indexOf('en') !== -1 ? (
         <EnglishIcon className="text-gray-80" />
       ) : (
         <ChineseIcon className="text-gray-80" />
