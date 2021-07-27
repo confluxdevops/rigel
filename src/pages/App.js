0df4717987368be1aba9b3287a3450d11a6115be
chao.zhou <zctocm@gmail.com>
2021-07-27 14:51:37 +0800

    feat(sentry): add Sentry service

--- src/pages/App.js
+++ src/pages/App.js
@@ -13,6 +13,20 @@ import Notfound from '../pages/Notfound'
 import {Web3ReactManager, Header, MobileFooter} from '../pages/components'
 import {Loading} from '../components'
 import {useIsMobile} from '../hooks'
+import * as Sentry from '@sentry/browser'
+import {Integrations} from '@sentry/tracing'
+import {IS_DEV} from '../utils'
+
+Sentry.init({
+  dsn: 'https://4d2e829843a54d21b43df7b20a8e93cf@o339419.ingest.sentry.io/5880699',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  environment: IS_DEV ? 'development' : 'production',
+})
 
 function App() {
   const isMobile = useIsMobile()
